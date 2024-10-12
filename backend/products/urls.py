0b4dec668dab405246352cefb1281fc004c94cc5
Riyan Jamil <riyanjamil220@gmail.com>
2024-10-12 11:39:03 +0500

    Looked into mixins by combining standard methods into cbv

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -2,13 +2,19 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('',ProductCreateAPIView.as_view(),name= 'product_create_api_view'),
+    # path('',ProductCreateAPIView.as_view(),name= 'product_create_api_view'),
+    path('',ProductMixinView.as_view(),name= 'product_create_api_view'),
 
     path('<int:pk>/',ProductDetailAPIView.as_view(),name= 'product_detail_api_view'),
+    # path('<int:pk>/',ProductMixinView.as_view(),name= 'product_detail_api_view'),
+
+
     path('<int:pk>/update',ProductUpdateAPIView.as_view(),name= 'product_detail_api_view'),
     path('<int:pk>/delete',ProductDeleteAPIView.as_view(),name= 'product_detail_api_view'),
 
+
     path('list/',ProductListCreateAPIView.as_view(),name= 'product_list_api_view'),
+    # path('list/',ProductMixinView.as_view(),name= 'product_list_api_view'),
 
     # path('<int:pk>/',product_alt_view,name= 'product_detail_api_view'),
     # path('',product_alt_view,name= 'product_create_api_view'),
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,5 +1,5 @@
 from rest_framework.response import Response
-from rest_framework import generics
+from rest_framework import generics , mixins
 from .models import Product
 from .serializers import ProductSerializer
 
@@ -41,6 +41,27 @@ class ProductDeleteAPIView(generics.DestroyAPIView):
     def perform_delete(self,instance):
         super().perform_delete(instance)
 
+
+class ProductMixinView(mixins.ListModelMixin,
+                       mixins.RetrieveModelMixin, 
+                       generics.GenericAPIView,
+                       mixins.CreateModelMixin):
+    
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def get(self,request,*args, **kwargs):
+        print(args,kwargs)
+        pk = kwargs.get('pk')
+
+        if pk is not None:
+            return self.retrieve(request,*args, **kwargs)
+
+        return self.list(request,*args, **kwargs)
+
+    def post(self, request,*args, **kwargs):
+        return self.create(request,*args, **kwargs)
 
 @api_view(["GET","POST"])
 def product_alt_view(request, pk = None,*args, **kwargs):
