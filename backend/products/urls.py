39f210615c8bce5a0ecf7d570cc174b93a4298fb
Riyan Jamil <riyanjamil220@gmail.com>
2024-10-14 23:22:51 +0500

    URLs, Reverse, & Serializers

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -5,15 +5,15 @@ urlpatterns = [
     path('',ProductCreateAPIView.as_view(),name= 'product_create_api_view'),
     # path('',ProductMixinView.as_view(),name= 'product_create_api_view'),
 
-    path('<int:pk>/',ProductDetailAPIView.as_view(),name= 'product_detail_api_view'),
+    path('<int:pk>/',ProductDetailAPIView.as_view(),name= 'product-detail'),
     # path('<int:pk>/',ProductMixinView.as_view(),name= 'product_detail_api_view'),
 
 
-    path('<int:pk>/update',ProductUpdateAPIView.as_view(),name= 'product_detail_api_view'),
+    path('<int:pk>/update',ProductUpdateAPIView.as_view(),name= 'product-edit'),
     path('<int:pk>/delete',ProductDeleteAPIView.as_view(),name= 'product_detail_api_view'),
 
 
-    path('list/',ProductListCreateAPIView.as_view(),name= 'product_list_api_view'),
+    path('list/',ProductListCreateAPIView.as_view(),name= 'product-list'),
     # path('list/',ProductMixinView.as_view(),name= 'product_list_api_view'),
 
     # path('<int:pk>/',product_alt_view,name= 'product_detail_api_view'),
